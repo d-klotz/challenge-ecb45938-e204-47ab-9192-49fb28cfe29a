90d60aebd4da701d74e6d903e3f4834e00ad4236
Daniel Felipe Klotz <danielfelipeklotz@gmail.com>
2024-01-21 09:13:30 -0300

    chore: format code

--- src/main/java/com/events/chalenge/models/InstitutionModel.java
+++ src/main/java/com/events/chalenge/models/InstitutionModel.java
@@ -1,44 +1,45 @@
 package com.events.chalenge.models;
 
+import com.events.chalenge.enums.IntitutionType;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.UUID;
-import com.events.chalenge.enums.IntitutionType;
 
 @Entity
-@Table(name="INSTITUTIONS")
+@Table(name = "INSTITUTIONS")
 public class InstitutionModel implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-    private String name;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
 
-    @Enumerated(EnumType.STRING)
-    private IntitutionType type;
+  private String name;
 
-    public UUID getId() {
-        return id;
-    }
+  @Enumerated(EnumType.STRING)
+  private IntitutionType type;
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public IntitutionType getType() {
-        return type;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setType(IntitutionType type) {
-        this.type = type;
-    }
+  public IntitutionType getType() {
+    return type;
+  }
+
+  public void setType(IntitutionType type) {
+    this.type = type;
+  }
 }
